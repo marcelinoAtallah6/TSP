911549f47922e9c1db1b7b559e7d5d1af13dd2ed
marcelinoAtallah6 <105923424+marcelinoAtallah6@users.noreply.github.com>
2024-09-01 13:13:40 +0300

    TSP

---
+++ src/app/layouts/full/full.component.ts
@@ -0,0 +1,96 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MatSidenav } from '@angular/material/sidenav';
+
+const MOBILE_VIEW = 'screen and (max-width: 768px)';
+const TABLET_VIEW = 'screen and (min-width: 769px) and (max-width: 1024px)';
+const MONITOR_VIEW = 'screen and (min-width: 1024px)';
+
+@Component({
+  selector: 'app-full',
+  templateUrl: './full.component.html',
+  styleUrls: [],
+})
+export class FullComponent implements OnInit {
+
+  @ViewChild('leftsidenav')
+  public sidenav: MatSidenav | any;
+
+  //get options from service
+  private layoutChangesSubscription = Subscription.EMPTY;
+  private isMobileScreen = false;
+  private isContentWidthFixed = true;
+  private isCollapsedWidthFixed = false;
+  private htmlElement!: HTMLHtmlElement;
+  public isOpened = false;
+
+  isMini: boolean = true;  // Tracks if the sidenav is in mini (collapsed) state
+  get isOver(): boolean {
+    return this.isMobileScreen;
+  }
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.htmlElement = document.querySelector('html')!;
+    this.layoutChangesSubscription = this.breakpointObserver
+      .observe([MOBILE_VIEW, TABLET_VIEW, MONITOR_VIEW])
+      .subscribe((state) => {
+        // SidenavOpened must be reset true when layout changes
+
+        this.isMobileScreen = state.breakpoints[MOBILE_VIEW];
+
+        this.isContentWidthFixed = state.breakpoints[MONITOR_VIEW];
+      });
+  }
+
+  ngOnInit(): void {}
+
+  ngOnDestroy() {
+    this.layoutChangesSubscription.unsubscribe();
+  }
+
+  toggleCollapsed() {
+    this.isContentWidthFixed = false;
+  }
+
+  // onSidenavClosedStart() {
+  //   this.isContentWidthFixed = false;
+  // }
+
+  // onSidenavOpenedChange(isOpened: boolean) {
+  //   this.isCollapsedWidthFixed = !this.isOver;
+  // }
+
+
+  onSidenavOpenedChange(isOpened: boolean) {
+    this.isCollapsedWidthFixed = !this.isOver;
+    this.isOpened = isOpened;
+    if (isOpened) {
+      this.isMini = true; // Open fully
+    }
+  }
+  onSidenavClosedStart() {
+    this.isContentWidthFixed = false;
+    if (!this.isOver) {
+      this.isMini = false; // Collapse to mini state
+    }
+  }
+
+  toggleSidenav(sidenav: any) {
+    // If the sidenav is in mini state or closed, open it fully
+    if (this.isMini || !sidenav.opened) {
+      this.isMini = false;  // Set to full state
+      // sidenav.open();       // Open the sidenav
+      // sidenav.isOver = true;
+      
+
+    } else {
+      // If it's fully opened, collapse to mini state
+      this.isMini = true;
+      // sidenav.close();
+      sidenav.isOver = false;
+    }
+  }
+
+
+}
---
+++ src/app/layouts/full/header/header.component.ts
@@ -0,0 +1,89 @@
+import {
+    Component,
+    Output,
+    EventEmitter,
+    Input,
+    ViewEncapsulation,
+    OnInit,
+  } from '@angular/core';
+  import { MatDialog } from '@angular/material/dialog';
+  
+  
+  @Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    encapsulation: ViewEncapsulation.None,
+  })
+  export class HeaderComponent implements OnInit {
+    @Input() showToggle = true;
+    @Input() toggleChecked = false;
+    @Output() toggleMobileNav = new EventEmitter<void>();
+    @Output() toggleMobileFilterNav = new EventEmitter<void>();
+    @Output() toggleCollapsed = new EventEmitter<void>();
+  
+    showFiller = false;    
+
+    menuItems = [
+      { label: 'Apps', icon: 'chevron-down', children: [] },
+      { label: 'Chat', icon: '', link: '/apps/chat' },
+      { label: 'Calendar', icon: '', link: '/apps/calendar' },
+      { label: 'Email', icon: '', link: '/apps/email/inbox' },
+    ];   
+  
+    subMenuItems = [
+      {
+        name: 'Chat Application',
+        description: 'Messages & Emails',
+        link: '/apps/chat',
+        icon: '/assets/images/svgs/icon-user-male.svg'
+      },
+      {
+        name: 'Todo App',
+        description: 'Completed',
+        link: '/apps/todo',
+        icon: '/assets/images/svgs/icon-user-male.svg'
+      },
+      {
+        name: 'Todo App',
+        description: 'Completed',
+        link: '/apps/todo',
+        icon: '/assets/images/svgs/icon-user-male.svg'
+      },
+      {
+        name: 'Todo App',
+        description: 'Completed',
+        link: '/apps/todo',
+        icon: '/assets/images/svgs/icon-user-male.svg'
+      },
+      {
+        name: 'Todo App',
+        description: 'Completed',
+        link: '/apps/todo',
+        icon: '/assets/images/svgs/icon-user-male.svg'
+      },
+      {
+        name: 'Todo App',
+        description: 'Completed',
+        link: '/apps/todo',
+        icon: '/assets/images/svgs/icon-user-male.svg'
+      },
+      
+      {
+        name: 'Todo App',
+        description: 'Completed',
+        link: '/apps/todo',
+        icon: '/assets/images/svgs/icon-user-male.svg'
+      },
+    ];
+
+      quickLinks = [
+        { label: 'Pricing Page', url: '/theme-pages/pricing' },
+        { label: 'Authentication Design', url: '/authentication/login' },
+      ];
+
+    constructor(public dialog: MatDialog) {}
+    ngOnInit(): void {
+
+     }
+  }
+  
